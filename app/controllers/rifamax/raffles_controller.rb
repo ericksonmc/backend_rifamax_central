3a816986881f89961c833c78c476fdc69fd1782f
Javier Diaz <javierdiazt406@gmail.com>
2024-11-11 14:21:54 -0400

    Fix: Hot patch to test why new seller_create endpoint is failing

--- app/controllers/rifamax/raffles_controller.rb
+++ app/controllers/rifamax/raffles_controller.rb
@@ -217,6 +217,8 @@ module Rifamax
     def create
       @rifamax_raffle = Rifamax::Raffle.new(rifamax_raffle_params)
       @rifamax_raffle.user_id = @current_user.id
+      @rifamax_raffle.sell_status = 1
+      @rifamax_raffle.expired_date = 3.days.from_now
       
       if @rifamax_raffle.save
         render json: @rifamax_raffle, status: :created, location: @rifamax_raffle
