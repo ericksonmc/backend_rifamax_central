4dd322dbbb7eea072071fd7215011e8b62769c73
Javier Diaz <javierdiazt406@gmail.com>
2025-01-16 00:17:46 -0400

    Fix: Deleted useless comment

--- app/models/rifamax/raffle.rb
+++ app/models/rifamax/raffle.rb
@@ -211,8 +211,6 @@ class Rifamax::Raffle < ApplicationRecord
     return { message: "Ticket has been sold!", tickets: Rifamax::TicketSerializer.new(tickets).object }
   end
 
-  # Shared::User.where(role: 'Taquilla').select { |taquilla| taquilla.rifero_ids.include?(@current_user.id) }.last.id
-
   private
 
   def self.statues_by_endpoint(endpoint)
