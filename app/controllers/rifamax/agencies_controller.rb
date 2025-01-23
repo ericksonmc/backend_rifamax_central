1d8ba5a4acde18748a6b1d48e1f2dfd72f9a9f7c
Javier Diaz <javierdiazt406@gmail.com>
2025-01-23 11:38:13 -0400

    Fix: Serializer of data of agencies

--- app/controllers/rifamax/agencies_controller.rb
+++ app/controllers/rifamax/agencies_controller.rb
@@ -2,6 +2,11 @@ class Rifamax::AgenciesController < ApplicationController
   before_action :authorize_request
 
   def index
-    render json: SerializableResource.new(@current_user.sellers, each_serializer: Rifamax::AgencySerializer).as_json, status: :ok
+    render json: serialize(Rifamax::Agency.all), status: :ok
   end
+
+  private
+
+  def serialize(data)
+    ActiveModelSerializers::SerializableResource.new(data, each_serializer: Rifamax::AgencySerializer).as_json
 end
