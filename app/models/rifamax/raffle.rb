0b6c69599ceeaa47f3a0421ef88a56ccdf1ea494
Javier Diaz <javierdiazt406@gmail.com>
2025-03-14 16:14:29 -0400

    Fix: Exception added when raffle is not payed

--- app/models/rifamax/raffle.rb
+++ app/models/rifamax/raffle.rb
@@ -212,7 +212,9 @@ class Rifamax::Raffle < ApplicationRecord
   
   def sell_all_tickets
     raise StandardError.new "You are not the seller! This incident will be reported to admins." unless self.seller_id == self.user_who_requested  
+    raise StandardError.new "You can't sold tickets if you don't pay the raffle first" if self.admin_status == 1
     raise StandardError.new "Tickets has been sold!" if self.sell_status == 'sold'
+    raise StandardError.new "Ticket list is empty" if tickets_ids.empty?
 
     self.update(
       sell_status: 2
@@ -227,6 +229,7 @@ class Rifamax::Raffle < ApplicationRecord
 
   def sell_some_tickets(tickets_ids = [])
     raise StandardError.new "You are not the seller! This incident will be reported to admins." unless self.seller_id == self.user_who_requested  
+    raise StandardError.new "You can't sold tickets if you don't pay the raffle first" if self.admin_status == 1
     raise StandardError.new "Tickets has been sold!" if self.sell_status == 'sold'
     raise StandardError.new "Ticket list is empty" if tickets_ids.empty?
 
