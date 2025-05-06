0cb13a521f29c8b5f86ae455292e9a96c49c0759
Javier Diaz <javierdiazt406@gmail.com>
2025-05-06 15:31:27 -0400

    Test: Testing if error was fixed

--- app/services/x100/ticket_selling_service.rb
+++ app/services/x100/ticket_selling_service.rb
@@ -39,7 +39,13 @@ module X100
         )
       end
     rescue => e
-      raise TicketSellingError, e
+      error_message = if e.respond_to?(:message) && e.message
+                        e.message
+                      else
+                        "Unknown error: #{e.inspect}"
+                      end
+      Rails.logger.error "Ticket selling failed: #{error_message}"
+      raise TicketSellingError, error_message
     end
 
     def self.sell_via_integrator(products:, money:, raffle:, integrator_id:, integrator_type:, user:)
@@ -69,7 +75,13 @@ module X100
         ) 
       end
     rescue => e
-      raise TicketSellingError, e
+      error_message = if e.respond_to?(:message) && e.message
+                        e.message
+                      else
+                        "Unknown error: #{e.inspect}"
+                      end
+      Rails.logger.error "Ticket selling failed: #{error_message}"
+      raise TicketSellingError, error_message
     end
 
     private_class_method
