e87c5031541bbb592cddcc2d96b0d8cb4251cd45
Javier Diaz <javierdiazt406@gmail.com>
2025-05-07 16:15:31 -0400

    Fix: Search order before buy

--- app/services/x100/ticket_selling_service.rb
+++ app/services/x100/ticket_selling_service.rb
@@ -300,6 +300,8 @@ module X100
 
       response = HTTParty.post(url, body: payload.to_json, headers: { 'Content-Type' => 'application/json' })
       
+      X100::Order.find_by(products: products, x100_raffle_id: raffle.id)
+
       raise IntegrationJobIsDownError.new({ 
         message: "Integration is down",
         code: response.code,
