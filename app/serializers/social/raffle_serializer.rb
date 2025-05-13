3444b9e1dc7458acf3993531ff30014fe82fe6a1
Javier Diaz <javierdiazt406@gmail.com>
2025-05-13 18:10:56 -0400

    Fix: Error of serializer on nested object of ad > url

--- app/serializers/social/raffle_serializer.rb
+++ app/serializers/social/raffle_serializer.rb
@@ -2,6 +2,10 @@ class Social::RaffleSerializer < ActiveModel::Serializer
   attributes :id, :ad, :title, :combos, :draw_type, :expired_date, :has_winners, :init_date, :limit, :money, :price_unit, :prizes, :raffle_type, :social_influencer_id, :status, :tickets_count, :winners, :created_at, :updated_at
 
   def ad
-    object.ad.url = "#{ENV['URL_BASE']}/#{object.ad.url}" if object.ad.present?
+    return unless object.ad.present?
+
+    object.ad.as_json.merge(
+      'url' => "#{ENV['url_base']}/#{object.ad.url}"
+    )
   end
 end
