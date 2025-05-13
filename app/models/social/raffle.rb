5a663d565420d6faf103757ca124c151724adc80
Javier Diaz <javierdiazt406@gmail.com>
2025-05-13 17:14:29 -0400

    Fix: Unexpected end in model Social Raffles was deleted

--- app/models/social/raffle.rb
+++ app/models/social/raffle.rb
@@ -106,7 +106,7 @@ class Social::Raffle < ApplicationRecord
             },
             comparison: {
               greater_than_or_equal_to: :init_date
-            }
+            }.
             if -> { draw_type == 'Limitada'}
 
   validates :social_influencer_id,
