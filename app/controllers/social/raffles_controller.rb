de0ee0ca83228ff2b93e415d150e435caddd66eb
Javier Diaz <javierdiazt406@gmail.com>
2025-06-20 17:34:21 -0400

    Feat: Added new params

--- app/controllers/social/raffles_controller.rb
+++ app/controllers/social/raffles_controller.rb
@@ -184,7 +184,7 @@ class Social::RafflesController < ApplicationController
   end
 
   def ad_params
-    params.permit(:ad, :rif, :dni, :bank_register, :receipts => [])
+    params.permit(:ad, :rif, :dni, :bank_register, :receipts)
   end
 
   def social_raffle_params
