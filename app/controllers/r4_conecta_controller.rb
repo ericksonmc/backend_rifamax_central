135740e33b84c2d95df863c95c617f17ea7fc595
Javier Diaz <javierdiazt406@gmail.com>
2025-06-25 17:27:54 -0400

    Feat: New validation to Social Raffles

--- app/controllers/r4_conecta_controller.rb
+++ app/controllers/r4_conecta_controller.rb
@@ -4,18 +4,17 @@ class R4ConectaController < ApplicationController
   def handshake
     @bank_signature_uuid = ENV.fetch('BANK_SIGNATURE_UUID', nil)
     @header_signature_uuid = request.headers['Authorization'].to_s
+    @commerce_phone = ENV.fetch('R4_CONECTA_COMMERCE_PHONE', nil)
 
     id_client = handshake_params[:idCliente]
     monto = handshake_params[:Monto]
     telefono_comercio = handshake_params[:TelefonoComercio]
 
     token_valid = @bank_signature_uuid == @header_signature_uuid
-    puts "Bank Signature UUID: #{@bank_signature_uuid}"
-    puts "Header Signature UUID: #{@header_signature_uuid}"
-    puts "ID Cliente: #{id_client}"
-    puts "Monto: #{monto}"
-    puts "Telefono Comercio: #{telefono_comercio}"
-    params_valid = id_client.present? && telefono_comercio.present?
+
+    phone_commerce_valid = @commerce_phone.present? && telefono_comercio == @commerce_phone
+
+    params_valid = id_client.present? && phone_commerce_valid
 
     return render json: { error: 'Not found' }, status: :not_found unless token_valid
 
