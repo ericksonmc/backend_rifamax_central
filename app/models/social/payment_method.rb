6218107c007e17dc3bc687417eee7dd764536047
Javier Diaz <javierdiazt406@gmail.com>
2025-06-27 21:40:35 -0400

    Fix: R4 notify payment fixed

--- app/models/social/payment_method.rb
+++ app/models/social/payment_method.rb
@@ -216,7 +216,7 @@ class Social::PaymentMethod < ApplicationRecord
   def notify_payment
     return true unless status == 'active' && payment == 'Pago Movil'
 
-    monto = (amount.to_f * Social::R4ConectaService.new.consultar_tasa_bcv["tipocambio"].to_f).to_s,
+    monto = (amount.to_f * Social::R4ConectaService.new.consultar_tasa_bcv["tipocambio"].to_f).to_s
     referencia = details["reference"].to_s
     telefono_emisor = "0#{details["phone"].gsub(/\D/, "")}"
     banco_emisor = BanksService.new.find_bank(details["bank"])[:code].slice(1, 4)
