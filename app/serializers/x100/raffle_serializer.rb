8fb0902d4f4591e1f8f2c394951c99d9ab453837
Javier Diaz <javierdiazt406@gmail.com>
2025-07-05 12:00:45 -0400

    Feat: Added lottery name in raffle serializer

--- app/serializers/x100/raffle_serializer.rb
+++ app/serializers/x100/raffle_serializer.rb
@@ -43,6 +43,7 @@ module X100
                :expired_date,
                :init_date,
                :prizes,
+               :lottery,
                :winners,
                :has_winners,
                :automatic_taquillas_ids,
@@ -56,6 +57,10 @@ module X100
 
     def created_at
       object.created_at.strftime('%d/%m/%Y %H:%M:%S')
+    end
+
+    def lottery
+      object.social_lottery.name
     end
 
     def ad
