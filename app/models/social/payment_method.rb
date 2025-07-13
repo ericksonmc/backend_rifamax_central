e7c8ab81e5b812bb34f5986629e8124f2248ddad
Javier Diaz <javierdiazt406@gmail.com>
2025-07-13 14:41:59 -0400

    Fix: Added amounts of buy

--- app/models/social/payment_method.rb
+++ app/models/social/payment_method.rb
@@ -272,7 +272,9 @@ class Social::PaymentMethod < ApplicationRecord
       if ((monto.to_f / self.fractions) - r4_result.to_f).abs <= 2
         $redis.del(redis_param)
         self.status = "accepted"
-        self.fraction_debt = self.fractions > 1 ? (self.amount / self.fractions) : 0.0
+        self.fraction_debt = self.fractions > 1 ? self.amount - (self.amount / self.fractions) : 0.0
+        self.fly_amounts ||= []
+        self.fly_amounts << (monto.to_f / self.fractions).round(2)
         self.is_fractionated = self.fractions > 1
         self.save
         true
