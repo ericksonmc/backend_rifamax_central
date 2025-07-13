9ebacf9e1e897674ed73f0281990dc6aedab0035
Javier Diaz <javierdiazt406@gmail.com>
2025-07-13 14:50:49 -0400

    Fix: Added amounts of buy

--- app/serializers/social/payment_method_serializer.rb
+++ app/serializers/social/payment_method_serializer.rb
@@ -43,7 +43,7 @@
 #  fk_rails_...  (social_raffle_id => social_raffles.id)
 #
 class Social::PaymentMethodSerializer < ActiveModel::Serializer
-  attributes :id, :payment, :amount, :serial, :currency, :status, :details, :client, :tickets, :raffle, :has_fly_amount, :fly_amounts, :fractions, :is_fractionated, :payment_rate, :created_at
+  attributes :id, :payment, :amount, :serial, :currency, :status, :details, :client, :tickets, :raffle, :has_fly_amount, :fly_amounts, :fractions, :fraction_debt, :is_fractionated, :payment_rate, :created_at
 
   def client
     object.social_client
