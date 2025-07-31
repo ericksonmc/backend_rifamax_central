03e84e7380e7e3953784485f65890cdc85094533
Javier Diaz <javierdiazt406@gmail.com>
2025-07-31 10:42:47 -0400

    Fix: Added @current_user authorization to only_influencers method

--- app/controllers/social/raffles_controller.rb
+++ app/controllers/social/raffles_controller.rb
@@ -2,7 +2,7 @@ class Social::RafflesController < ApplicationController
   include Pagy::Backend
 
   before_action :set_social_raffle, only: %i[ show update destroy ]
-  before_action :authorize_request, only: %i[ index show list_dashboard profit_dashboard create update destroy only_lotteries confirm reject ]
+  before_action :authorize_request, only: %i[ index show list_dashboard profit_dashboard create update destroy only_influencers only_lotteries add_content confirm reject ]
   before_action :only_lotteries, only: %i[confirm reject]
   before_action :only_influencers, only: %i[add_content]
   
