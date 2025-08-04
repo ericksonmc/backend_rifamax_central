30567c3ef0bc41ad64c14037670fb7b9e88d2107
Javier Diaz <javierdiazt406@gmail.com>
2025-08-04 18:29:33 -0400

    Fix: Making just one route

--- config/routes.rb
+++ config/routes.rb
@@ -27,12 +27,7 @@ Rails.application.routes.draw do
     post 'login', to: 'authentication#social_login'
     post 'auth/refresh', to: 'authentication#social_refresh'
     
-    resources :wa_webhook do
-      post '/', to: 'wa_webhook#webhook'
-      post 'chats_upsert', on: :collection
-      post 'messages_upsert', on: :collection
-      post 'send_message', on: :collection
-    end
+    resources :wa_webhook
     
     resources :networks
     resources :lotteries do 
