f392a1db788efc0f997f79ea788ca2aec39a2581
Javier Diaz <javierdiazt406@gmail.com>
2025-08-07 18:04:35 -0400

    Fix: Set callback before action to function

--- app/controllers/social/raffles_controller.rb
+++ app/controllers/social/raffles_controller.rb
@@ -1,7 +1,7 @@
 class Social::RafflesController < ApplicationController
   include Pagy::Backend
 
-  before_action :set_social_raffle, only: %i[ filter_by_custom_link ]
+  before_action :set_social_raffle_by_custom_link, only: %i[ filter_by_custom_link ]
   before_action :set_social_raffle, only: %i[ show update destroy ]
   before_action :authorize_request, only: %i[ index list_dashboard profit_dashboard create update destroy only_influencers only_lotteries add_content confirm reject ]
   before_action :only_lotteries, only: %i[confirm reject]
