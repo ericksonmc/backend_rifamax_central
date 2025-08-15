0d3e002a8e697514ca94c197cedfa8caad27bd6e
Javier Diaz <javierdiazt406@gmail.com>
2025-08-15 16:28:59 -0400

    Fix: Adjust presence of element

--- app/models/social/context.rb
+++ app/models/social/context.rb
@@ -12,7 +12,6 @@ class Social::Context < ApplicationRecord
 
   # Validations
   validates :key, presence: true, uniqueness: true
-  validates :context, presence: true
 
   # Scopes
   scope :by_key, ->(key) { where(key: key) }
