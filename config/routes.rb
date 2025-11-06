0d0d34bf1fd594d6a7aebeae60d4d9079ec835ec
Javier Diaz <javierdiazt406@gmail.com>
2025-11-06 18:05:55 -0400

    Feat: Toggle Fractions endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Rails.application.routes.draw do
       get 'list_dashboard', on: :collection
       put 'add_documents', on: :collection
       put 'add_content', on: :collection
-      put 'toggle_fractions', on: :collection
+      post 'toggle_fractions', on: :member
       get 'actives', on: :collection
       get 'filter_by_custom_link', on: :collection
       get 'info', on: :member
