0ebff8e8b2477b5b19889a06ddeefe6c60fe2d82
Erickson C <erick2109@gmail.com>
2026-05-21 16:27:33 -0400

    Subiendo cambios en la valdiacion de venta de tickets

--- app/models/rifamax/raffle.rb
+++ app/models/rifamax/raffle.rb
@@ -220,7 +220,7 @@ class Rifamax::Raffle < ApplicationRecord
   
   def sell_all_tickets
     raise StandardError.new "You are not the seller! This incident will be reported to admins." unless self.seller_id == self.user_who_requested  
-    raise StandardError.new "You can't sold tickets if you don't pay the raffle first" unless admin_status == 'payed'
+    validate_payment_before_selling!
     raise StandardError.new "Tickets has been sold!" if self.sell_status == 'sold'
     raise StandardError.new "Ticket list is empty" if tickets_ids.empty?
 
@@ -237,7 +237,7 @@ class Rifamax::Raffle < ApplicationRecord
 
   def sell_some_tickets(tickets_ids = [])
     raise StandardError.new "You are not the seller! This incident will be reported to admins." unless self.seller_id == self.user_who_requested  
-    raise StandardError.new "You can't sold tickets if you don't pay the raffle first" unless admin_status == 'payed'
+    validate_payment_before_selling!
     raise StandardError.new "Tickets has been sold!" if self.sell_status == 'sold'
     raise StandardError.new "Ticket list is empty" if tickets_ids.empty?
 
@@ -257,6 +257,13 @@ class Rifamax::Raffle < ApplicationRecord
   end
 
   private
+
+  def validate_payment_before_selling!
+    return if admin_status == 'payed'
+    return if expired_date.present? && expired_date > Date.current
+
+    raise StandardError.new "You can't sold tickets if you don't pay the raffle first"
+  end
 
   PAYMENT_ACTIONS = %w[pay confirm refund].freeze
   MISSING_SPJ_TOKEN = "Can't perform this action without a SPJ token".freeze
